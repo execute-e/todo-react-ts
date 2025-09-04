f18dfb1febbf2dbb39aa63e0d6cad7b0af69f1b8
executed <execute.eee@yandex.ru>
2025-09-04 16:01:06 +0300

    delete persist from sort store

--- src/store/useSort.ts
+++ src/store/useSort.ts
@@ -1,20 +1,16 @@
 import { create } from "zustand";
 import { methods } from "./useStore";
-import { createJSONStorage, persist } from "zustand/middleware";
 
 type useSort = {
     sortMethod: methods;
     setSortMethod: (sortMethod: methods) => void;
 }
 
-export const useSort = create<useSort>()(persist((set) => ({
+export const useSort = create<useSort>()((set) => ({
     sortMethod: 'byPriority',
     setSortMethod: (sortMethod) => {
         set({
             sortMethod,
         })
     }
-}), {
-    name: 'todo-sort-method',
-    storage: createJSONStorage(() => localStorage),
 }))
